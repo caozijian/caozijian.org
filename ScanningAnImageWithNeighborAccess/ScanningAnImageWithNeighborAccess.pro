182546e1954dc403096d0e87c6e8d095638bc063
Zijian Cao <caozijian.cn@gmail.com>
2015-02-24 22:45:33 +0800

    Sharpen result have some problem.

--- ScanningAnImageWithNeighborAccess/ScanningAnImageWithNeighborAccess.pro
+++ ScanningAnImageWithNeighborAccess/ScanningAnImageWithNeighborAccess.pro
@@ -3,7 +3,8 @@
 # Project created by QtCreator 2015-02-24T12:24:33
 #
 #-------------------------------------------------
-
+# $2.6 Scanning an image with neighbor access
+# Oh, I think this code have some bug. Because the result image is not same as figure 2.4 on page 48.
 QT       += core
 
 QT       -= gui
@@ -16,3 +17,10 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp
+INCLUDEPATH += /usr/local/include \
+                /usr/local/include/opencv \
+                /usr/local/include/opencv2
+
+LIBS += /usr/local/lib/libopencv_highgui.so \
+        /usr/local/lib/libopencv_core.so    \
+        /usr/local/lib/libopencv_imgproc.so
--- ScanningAnImageWithNeighborAccess/main.cpp
+++ ScanningAnImageWithNeighborAccess/main.cpp
@@ -1,8 +1,40 @@
 #include <QCoreApplication>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv.hpp>
 
-int main(int argc, char *argv[])
+void sharpen(const cv::Mat &image, cv::Mat &result);
+
+int main()
 {
-    QCoreApplication a(argc, argv);
 
-    return a.exec();
+    cv::Mat image = cv::imread("../../images/boldt.jpg");
+    // RGB to Gray
+    cv::cvtColor(image,image,CV_BGR2GRAY);
+    cv::namedWindow("original");
+    cv::imshow("original",image);
+    sharpen(image,image);
+    cv::namedWindow("sharpen");
+    cv::imshow("sharpen",image);
+    cv::waitKey(0);
+    return 1;
+}
+
+void sharpen(const cv::Mat &image, cv::Mat &result){
+    result.create(image.size(),image.type());
+    for (int j = 1; j < image.rows-1;j++){
+        const uchar* previous = image.ptr<const uchar>(j-1);
+        const uchar* current = image.ptr<const uchar>(j);
+        const uchar* next = image.ptr<const uchar>(j+1);
+
+        uchar* output = result.ptr<uchar>(j);
+        for (int i = 1; i < image.cols-1;i++){
+            *output++ = cv::saturate_cast<uchar>(5*current[i]-current[i-1]-current[i+1]-previous[i]-next[i]);
+        }
+    }
+    //
+    result.row(0).setTo(cv::Scalar(0));
+    result.row(result.rows-1).setTo(cv::Scalar(0));
+    result.col(0).setTo(cv::Scalar(0));
+    result.col(result.cols-1).setTo(cv::Scalar(0));
 }
